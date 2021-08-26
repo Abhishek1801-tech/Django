a3df70dd06e98dab56ad581d4c3382350fe8956f
Abhishek1801-tech <80214413+Abhishek1801-tech@users.noreply.github.com>
2021-08-26 18:50:52 +0530

    updated setting.py

--- PandeyShop/settings.py
+++ PandeyShop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-pzex7jb46@f0_7&k!&g1bj2dtl@uyngub#7ij4ve3*&=u$m8ty"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
